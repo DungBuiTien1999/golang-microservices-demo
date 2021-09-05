dd260b19a1b756f07e35619af6f93c55465fc55b
DungBuiTien1999 <nguyentonhi199@gmail.com>
2021-09-05 17:15:49 +0700

    first commit

---
+++ data/products.go
@@ -0,0 +1,93 @@
+package data
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"time"
+)
+
+type Product struct {
+	ID          int     `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float32 `json:"price"`
+	SKU         string  `json:"sku"`
+	CreatedOn   string  `json:"-"`
+	UpdatedOn   string  `json:"-"`
+	DeletedOn   string  `json:"-"`
+}
+
+type Products []*Product
+
+func (p *Products) ToJSON(w io.Writer) error {
+	e := json.NewEncoder(w)
+	return e.Encode(p)
+}
+
+func (p *Product) FromJSON(r io.Reader) error {
+	e := json.NewDecoder(r)
+	return e.Decode(p)
+}
+
+func GetProducts() Products {
+	return productList
+}
+
+func AddProduct(p *Product) {
+	p.ID = getNextID()
+	p.CreatedOn = time.Now().UTC().String()
+	p.UpdatedOn = time.Now().UTC().String()
+
+	productList = append(productList, p)
+}
+
+func getNextID() int {
+	lp := productList[len(productList)-1]
+	return lp.ID + 1
+}
+
+func UpdateProduct(id int, p *Product) error {
+	fp, pos, err := findProductById(id)
+	if err != nil {
+		return err
+	}
+
+	p.ID = id
+	p.CreatedOn = fp.CreatedOn
+	p.UpdatedOn = time.Now().UTC().String()
+	productList[pos] = p
+	return nil
+}
+
+var ErrProductNotFound = fmt.Errorf("Product not found")
+
+func findProductById(id int) (*Product, int, error) {
+	for index, p := range productList {
+		if p.ID == id {
+			return p, index, nil
+		}
+	}
+	return nil, -1, ErrProductNotFound
+}
+
+var productList = []*Product{
+	{
+		ID:          1,
+		Name:        "Latte",
+		Description: "Frothy milky coffee",
+		Price:       2.45,
+		SKU:         "abc123",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+	{
+		ID:          2,
+		Name:        "Espresso",
+		Description: "Short and strong coffee without milk",
+		Price:       1.19,
+		SKU:         "asdas",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+}
---
+++ handlers/products.go
@@ -0,0 +1,102 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+	"regexp"
+	"strconv"
+
+	"github.com/product-api/data"
+)
+
+type Products struct {
+	l *log.Logger
+}
+
+func NewProducts(l *log.Logger) *Products {
+	return &Products{l}
+}
+
+func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		p.getProducts(rw, r)
+		return
+	}
+
+	if r.Method == http.MethodPost {
+		p.addProduct(rw, r)
+		return
+	}
+
+	if r.Method == http.MethodPut {
+		p.l.Println("PUT")
+		// expect the id in the URL
+		rg := regexp.MustCompile(`/([0-9]+)`)
+		g := rg.FindAllStringSubmatch(r.URL.Path, -1)
+
+		if len(g) != 1 && len(g[0]) != 2 {
+			http.Error(rw, "Invalid URL", http.StatusBadRequest)
+			return
+		}
+
+		idString := g[0][1]
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			http.Error(rw, "Cannot convert id into number", http.StatusBadRequest)
+			return
+		}
+
+		p.updateProduct(id, rw, r)
+		return
+	}
+
+	// catch all
+	rw.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle GET Products")
+
+	lp := data.GetProducts()
+	err := lp.ToJSON(rw)
+	if err != nil {
+		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
+	}
+}
+
+func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle POST Products")
+
+	prod := &data.Product{}
+
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to marshal json", http.StatusBadRequest)
+	}
+
+	p.l.Printf("Prod: %#v", prod)
+	data.AddProduct(prod)
+}
+
+func (p *Products) updateProduct(id int, rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle PUT Products")
+
+	prod := &data.Product{}
+
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to marshal json", http.StatusBadRequest)
+	}
+
+	p.l.Printf("Prod: %#v", prod)
+
+	err = data.UpdateProduct(id, prod)
+	if err == data.ErrProductNotFound {
+		http.Error(rw, "Product not found", http.StatusNotFound)
+		return
+	}
+	if err != nil {
+		http.Error(rw, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+}
